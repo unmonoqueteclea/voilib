1592772e706156cc6849d50858e22e588774b1ec
unmonoqueteclea <gonzalez@bigml.com>
2024-12-01 17:22:33 +0100

    feat: add shutting down announcement

--- frontend/src/lib/Hero.svelte
+++ frontend/src/lib/Hero.svelte
@@ -2,6 +2,31 @@
   <div class="hero-content text-center">
     <div class="max-w-3xl px-8">
       <h1 class="text-2xl md:text-3xl font-bold">The search engine for audio content</h1>
+
+
+      <div class="card bg-red-800 text-white mt-8 w-full shadow-xl">
+	<div class="card-body">
+	  <h2 class="card-title"> 👋 Time to say goodbye </h2>
+	  <p class="text-left">
+	    <span>
+	      Voilib  will be <span class="font-bold">shutting down
+		permanently</span> on December 14th, 2024.
+	    </span>
+	    <br/>
+	    <span class="mt-8 pt-8">
+	      However, the project is open-source, and you can continue using
+	      it by self-hosting.
+
+	      For instructions, visit the
+	      <a class="underline"
+		 href="https://github.com/unmonoqueteclea/voilib">GitHub repository</a>
+	      Thank you for your support!
+	    </span>
+
+	  </p>
+	</div>
+      </div>
+
       <p class="py-6 text-xl">
 	<span class="text-base mr-1">🔍</span> Semantic search in
 	thousands of minutes of
