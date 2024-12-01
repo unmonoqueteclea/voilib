16e5722b923ab1212929c6de0f5a8f45850665ac
unmonoqueteclea <gonzalez@bigml.com>
2024-12-01 17:37:55 +0100

    fix: avoid error with API port 80

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,12 @@
 const BASE = import.meta.env.VITE_API_HOST || 'http://localhost'
 const PORT = import.meta.env.VITE_API_PORT || '81'
 const PREFIX = import.meta.env.VITE_API_PREFIX || ''
-export const API_URL =  BASE + ':' + PORT + PREFIX
+
+
+const isDefaultPort = (url, port) => {
+  const isHttp = url.startsWith('http://') && port === '80';
+  const isHttps = url.startsWith('https://') && port === '443';
+  return isHttp || isHttps;
+};
+
+export const API_URL = isDefaultPort(BASE, PORT) ? BASE + PREFIX : BASE + ':' + PORT + PREFIX;
